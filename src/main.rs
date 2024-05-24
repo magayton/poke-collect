1469088bdb6dd9ec252561309237032acf3b7d67
Mathis Gayton <mathis.gayton@gmail.com>
2024-05-24 22:01:52 +0200

    First draft to retrieve api data

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,29 @@
-use reqwest::{Client, ClientBuilder};
+use reqwest::ClientBuilder;
+use serde::Deserialize;
+
+
+#[derive(Deserialize, Debug)]
+struct TypeInfo {
+    name: String,
+    url: String,
+}
+
+
+#[derive(Deserialize, Debug)]
+struct PokemonType {
+    slot: u8,
+    #[serde(rename = "type")]
+    type_info: TypeInfo,
+}
+
+#[derive(Deserialize, Debug)]
+struct Pokemon {
+    name: String,
+    height: u32,
+    weight: u32,
+    base_experience: u32,
+    types: Vec<PokemonType>,
+}
 
 #[tokio::main]
 async fn main() {
@@ -6,8 +31,12 @@ async fn main() {
         .connect_timeout(std::time::Duration::from_secs(10))
         .build().unwrap_or_default();
 
-    let result = client.get(String::from("https://pokeapi.co/api/v2/pokemon/ditto")).build()
-        .send().await;
-
-    println!("{:?}", result);
+    let rep = client.get(String::from("https://pokeapi.co/api/v2/pokemon/ditto")).send().await.expect("Request failed during get");
+    if rep.status().is_success() {
+        let poke: Result<Pokemon, reqwest::Error> = rep.json().await;
+        println!("{:?}",poke.unwrap());
+    }
+    else {
+        println!("Err : {}", rep.status());
+    }
 }
