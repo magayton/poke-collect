4c202366179e7bee85209350c6ec23022a383ee4
magayton <mathis.gayton@gmail.com>
2024-06-04 18:50:07 +0200

    Add first draft for shiny catch

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
-use std::env;
+use std::{env, sync::{atomic::{AtomicBool, Ordering}, mpsc, Arc}, thread};
 
 use clap::{arg, command, Arg, ArgMatches, Command};
 use dotenv::dotenv;
 use reqwest::{Client, ClientBuilder};
 use serde_json::{from_value, Value};
+use sha2::{Sha256, Digest};
 use sqlx::{migrate, Pool, Postgres, Row};
 
 mod poke;
 mod sprite;
 use poke::{DbPoke, Pokemon, PokemonType, Stat};
-
-// TODO : multi catch command for async multi queries (provide a file with pokemon name and query them all)
+use tokio::task;
 
 #[tokio::main]
 async fn main() {
@@ -48,8 +48,12 @@ async fn main() {
         Command::new("shiny")
         .about("Catch a shiny version")
         .arg(
-        arg!(<POKE> "pokemon name").required(true)
-    ))
+            arg!(<POKE> "pokemon name").required(true)
+        )
+        .arg(
+            arg!(<DIFFICULTY> "Number of leading 0").required(true)
+        )
+)
     .subcommand(
         Command::new("collection")
         .about("Show your pokemon collection")
@@ -70,8 +74,14 @@ async fn main() {
     match cli_result.subcommand() {
         Some(("catch", sub_matches)) => catch_pokemon(client, sub_matches.get_one::<String>("POKE").unwrap(), &db_pool).await,
         Some(("info", sub_matches)) => info_pokemon(sub_matches.get_one::<String>("POKE").unwrap(), &db_pool).await,
-        Some(("shiny", sub_matches)) => println!("Ca veut tenter le shiny sur le {:?}", sub_matches.get_one::<String>("POKE")),
+        Some(("shiny", sub_matches)) => shiny_pokemon(sub_matches.get_one::<String>("POKE").unwrap(), sub_matches.get_one::<String>("DIFFICULTY").unwrap(), &db_pool),
         Some(("collection", sub_matches)) => println!("Ca veut voir la collection {:?}", sub_matches.get_one::<String>("GEN")),
+        Some(("multi-catch", sub_matches)) => {
+            if let Some(names) = sub_matches.get_many::<String>("names") {
+                let names: Vec<String> = names.map(|name| name.to_string()).collect();
+                multi_catch_pokemon(client, names, &db_pool).await;
+            }
+        }
         _ => unreachable!(),
     }
 }
@@ -118,11 +128,73 @@ async fn info_pokemon(name: &String, db_co: &Pool<Postgres>) {
     println!("{}", pokemon);
 }
 
-// 
-fn shiny_pokemon(name: String, db_co: &Pool<Postgres>) {
-    
+fn shiny_pokemon(name: &String, zeros: &String, db_co: &Pool<Postgres>) {
+    let (tx_result, rx_result) = mpsc::channel();
+    let mut handles = vec![];
+    let solution_found = Arc::new(AtomicBool::new(false));
+
+    for i in 0..8 {
+        let tx_result = tx_result.clone();
+        let zeros:usize = zeros.clone().parse().unwrap();
+        let solution_found = solution_found.clone();
+        let handle = thread::spawn(move || {
+            let mut counter = i as u64;
+            loop {
+                let hash = generate_hash(counter);
+                if is_shiny(&hash, &zeros) {
+                    solution_found.store(true, Ordering::SeqCst);
+                    tx_result.send(counter).unwrap();
+                    return;
+                }
+                counter += 8;  
+
+                if solution_found.load(Ordering::SeqCst) {
+                    break;
+                } 
+            }
+        });
+        handles.push(handle);
+    }
+
+    drop(tx_result);
+
+    if let Ok(result) = rx_result.recv() {
+        println!("J'ai trouvé le shiny avec le : {}", result);
+        for handle in handles {
+            handle.join().unwrap();
+        }
+    }
+    else {
+        println!("error la team");
+    }
+}
+
+fn generate_hash(input: u64) -> String {
+    let mut hasher = Sha256::new();
+    hasher.update(input.to_string());
+    let result = hasher.finalize();
+    println!("{:x}", result);
+    format!("{:x}", result)
+}
+
+fn is_shiny(hash: &str, zeros: &usize) -> bool {
+    hash.starts_with(&"0".repeat(*zeros))
 }
 
 fn collection_pokemon(gen: Option<u8>, db_co: &Pool<Postgres>) {
     
+}
+
+async fn multi_catch_pokemon(client: Client, names: Vec<String>, db_co: &Pool<Postgres>) {
+    let poke = names.into_iter().map(|name| {
+        let client = client.clone();
+        let db_co = db_co.clone();
+        task::spawn(async move {
+            catch_pokemon(client, &name, &db_co).await;
+        })
+    });
+
+    for poke in poke {
+        poke.await.unwrap();
+    }
 }
